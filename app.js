328db5112578ac0abf274979f5383a5e07a62363
Giovana Pracanico <125595708+projgipracanico@users.noreply.github.com>
2024-05-06 20:50:13 +0000

    "main"

--- app.js
+++ app.js
@@ -20,7 +20,31 @@ function inserir(lg,sn,mail) {
         console.log("Cadastro com sucesso!")
     })
 }
+//apagar dados = delete from nometabela
+function apagar(id){
+    let sql = "delete from user where id = ?"
+    connection.query (sql,id,function(error,results,fields){
+        if (error) throw error
+        console.log("apagado com sucesso!")
+    })
+}
 
-inserir ("Giovana", "12345", "gigica@gmail.com")
+//inserir ("Giovana", "12345", "gigica@gmail.com")
 
 connection.end()
+
+
+//slecionar dados por id - select nometabela
+function selecionarTudo(id){
+    let sql = "slect * from user where id =?
+    connection.query(sql, function(errror,results,fields){
+        for (let i=;i<results.length;i++){
+        if (error) throw error
+        console.log("Selecionado: " + results[i].id + ":" + results[i].login +
+                                                                  ":" + results[i].email )
+        }
+ })
+}
+
+
+selecionarTudo()
